bc150853032f5beb8971c717ff4a34260f4e0081
lighnio <charlesvas16@outlook.com>
2022-10-26 12:42:49 -0600

    base structure for media player ready

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit {
+   mockCover: any = {
+    cover:'https://i1.sndcdn.com/artworks-000247627460-1hqnjr-t500x500.jpg',
+    album:'Valentino Ft MTZ Manuel Turizo (Video Oficial)"',
+    name:'Bésame💋',
+
+   }
 
   constructor() { }
 
