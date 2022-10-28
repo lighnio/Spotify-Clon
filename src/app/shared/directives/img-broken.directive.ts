f45df608ba5ffd9708c65d002d0030577aff78da
lighnio <charlesvas16@outlook.com>
2022-10-27 19:02:48 -0600

    added validator for empty customImg property

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,13 +1,16 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
+  @Input() customImg: string = ''
+
   @HostListener('error') errorHandler(): void {
+    const localImg = '../../../../assets/images/spotify-logo.png'
     console.log('This img is broken --->', this.theHost);
     const nativeElement = this.theHost.nativeElement;
-    nativeElement.src = '../../../../assets/images/spotify-logo.png'
+    this.customImg.length > 0 ? nativeElement.src = this.customImg : nativeElement.src = localImg
     
   }
 
