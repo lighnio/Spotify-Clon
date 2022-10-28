93e8b67227a8b3148f14f435078993cf5fe4a37c
lighnio <charlesvas16@outlook.com>
2022-10-28 00:45:47 -0600

    basic structure for login

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPagesComponent } from './pages/auth-pages/auth-pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: AuthPagesComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/auth/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/auth/pages/auth-pages/auth-pages.component.ts
+++ src/app/modules/auth/pages/auth-pages/auth-pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-pages',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-pages.component.css']
 })
 export class AuthPagesComponent implements OnInit {
-
+  formLogin: FormGroup = new FormGroup({})
   constructor() { }
 
   ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12)
+      ]),
+    })
+  }
+
+  sendLogin(): void {
+    const body = this.formLogin.value
+    console.log(body)
+    
   }
 
 }
